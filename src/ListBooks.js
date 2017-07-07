a5cdbdac2292049165610111b721af5cf05823fa
jimmy <jimmyjconnelly@gmail.com>
2017-07-08 00:06:14 +0100

    added PropTypes to BookShelf component

--- src/ListBooks.js
+++ src/ListBooks.js
@@ -10,56 +10,55 @@ import PropTypes from 'prop-types'
 
 
 class BookShelf extends Component{
-
-    render(){
-      
-       const listFormattedName = this.props.listFormattedName
-       const books = this.props.books
-
-      
-
-       return (<div className="bookshelf">
-                  <h2 className="bookshelf-title">{listFormattedName}</h2>
-
        
-       <ol className="books-grid">
-           
-        {books.filter ((book) =>  book.shelf === this.props.listName ).map(book => (
+  static PropTypes = {
+      
+      handleChange : PropTypes.func.isRequired,
+      moveToCurrentlyReading : PropTypes.func.isRequired,
+      moveToRead : PropTypes.func.isRequired,
+      moveToWantToRead : PropTypes.func.isRequired,
+      books : PropTypes.array.isRequired,
+      listFormattedName : PropTypes.string.isRequired,
+      listName : PropTypes.string.isRequired
+
+  }
+
+
+  render(){    
+    const listFormattedName = this.props.listFormattedName
+    const books = this.props.books      
+    return (<div className="bookshelf">
+            <h2 className="bookshelf-title">{listFormattedName}</h2>   
+    <ol className="books-grid">
           
-            <li key={book.title}>
-                <div className="book">
-                    <div className="book-top">
-                        <div className="book-cover" style={{ width: 128, height: 193, backgroundImage: "url(" + book.imageLinks.thumbnail + ")" }}></div>
-                           
-                           
-                          <div className="book-shelf-changer">                   
-                            <select  className="browser-default" onClick={(event) => this.props.handleChange(event, book)} >
-                                <option value="none" disabled>Move to...</option>
-                                <option value="currentlyReading">Currently Reading</option>
-                                <option value="wantToRead">Want to Read</option>
-                                <option value="read">Read</option>
-                                <option value="none">None</option>
-                            </select>
-                          </div>
-                           
-                           
-                           
-                           
-                            
-                    </div>
-                    <div className="book-title">{book.title}</div>
-                    <div className="book-authors">{book.authors}</div>
-                </div>
-            </li>
-        ))}     
-        </ol>
-         <div className="open-search">
-          <Link to="/add-a-book">Add a book</Link>
-        </div>  
-    </div>  
+    {books.filter ((book) =>  book.shelf === this.props.listName ).map(book => (
         
-    )   
-    }
+      <li key={book.title}>
+        <div className="book">
+          <div className="book-top">
+            <div className="book-cover" style={{ width: 128, height: 193, backgroundImage: "url(" + book.imageLinks.thumbnail + ")" }}></div>
+            <div className="book-shelf-changer">                   
+              <select  className="browser-default" onClick={(event) => this.props.handleChange(event, book)} >
+                <option value="none" disabled>Move to...</option>
+                <option value="currentlyReading">Currently Reading</option>
+                <option value="wantToRead">Want to Read</option>
+                <option value="read">Read</option>
+                <option value="none">None</option>
+              </select>
+            </div>
+          </div>
+          <div className="book-title">{book.title}</div>
+          <div className="book-authors">{book.authors}</div>
+        </div>
+      </li>
+    ))}     
+    </ol>
+    <div className="open-search">
+    <Link to="/add-a-book">Add a book</Link>
+    </div>  
+  </div>  
+      
+  )}
 }
 
 class BookCase extends Component{
@@ -73,9 +72,6 @@ class BookCase extends Component{
 
 
   handleChange = (event, book) =>{
-    //console.log(event.target.value)
-    console.log('hello')
-  //  alert(event.target.value + "book id = " + book.id + " and book list = " + book.shelf)
     if(event.target.value === 'currentlyReading'){
       this.moveToCurrentlyReading(book)
     }
@@ -117,7 +113,7 @@ class BookCase extends Component{
         </div>        
         <div className="list-books-content">
           <div>
-                       
+
             <BookShelf handleChange={this.handleChange} moveToCurrentlyReading={this.moveToCurrentlyReading}   moveToRead={this.moveToRead} moveToWantToRead={this.moveToWantToRead}  books={this.state.books} listFormattedName='Currently Reading' listName='currentlyReading'/>
             <BookShelf handleChange={this.handleChange} moveToCurrentlyReading={this.moveToCurrentlyReading}   moveToRead={this.moveToRead} moveToWantToRead={this.moveToWantToRead}  books={this.state.books}  listFormattedName='Read' listName='read'/>
             <BookShelf  handleChange={this.handleChange} moveToCurrentlyReading={this.moveToCurrentlyReading}   moveToRead={this.moveToRead} moveToWantToRead={this.moveToWantToRead} books={this.state.books}   listFormattedName='Want to Read' listName='wantToRead'/>
