058a427094ce2dcf1acd514bbab28c573c5b2b0a
jimmy <jimmyjconnelly@gmail.com>
2017-07-07 23:47:19 +0100

    added react router

--- src/App.js
+++ src/App.js
@@ -2,34 +2,26 @@ import React from 'react'
 //import * as BooksAPI from './BooksAPI'
 import './App.css'
 import BookCase from './ListBooks'
+import SearchBooks from './SearchBooks'
+import {BrowserRouter} from 'react-router-dom'
+import {Route} from 'react-router-dom'
 
 
 
 
 
 class BooksApp extends React.Component {
-  state = {
-    /**
-     * TODO: Instead of using this state variable to keep track of which page
-     * we're on, use the URL in the browser's address bar. This will ensure that
-     * users can use the browser's back and forward buttons to navigate between
-     * pages, as well as provide a good URL they can bookmark and share.
-     */
-    showSearchPage: true
-  }
-  
-
+ 
   render() {
     return (
+    <BrowserRouter>
       <div className="app">
-        <BookCase/>
-
-
+        <Route path="/add-a-book" exact component={SearchBooks}/> 
+        <Route path="/" exact component={BookCase}/>      
       </div>
+    </BrowserRouter>
     )
-      
-}
-
+  }
 }
 
 export default BooksApp
--- src/ListBooks.js
+++ src/ListBooks.js
@@ -1,7 +1,7 @@
 import React ,{Component} from 'react'
 import *  as BooksAPI  from './BooksAPI'
-//import {Link} from 'react-router-dom'
-//import PropTypes from 'prop-types'
+import {Link} from 'react-router-dom'
+import PropTypes from 'prop-types'
 //import escapeRegExp from 'escape-string-regexp'
 //import sortBy from 'sort-by'
 
@@ -12,7 +12,7 @@ import *  as BooksAPI  from './BooksAPI'
 class BookShelf extends Component{
 
     render(){
-       const listName = this.props.listName
+      
        const listFormattedName = this.props.listFormattedName
        const books = this.props.books
 
@@ -52,81 +52,81 @@ class BookShelf extends Component{
                 </div>
             </li>
         ))}     
-        </ol>  
-    </div>      
+        </ol>
+         <div className="open-search">
+          <Link to="/add-a-book">Add a book</Link>
+        </div>  
+    </div>  
+        
     )   
     }
 }
 
 class BookCase extends Component{
-    state = {books : []
-}
+  state = {books : []}
 
-componentDidMount(){
-  BooksAPI.getAll().then((books) =>{
-    this.setState({books})
-  })
-}
+  componentDidMount(){
+    BooksAPI.getAll().then((books) =>{
+      this.setState({books})
+    })
+  }
 
 
-     handleChange = (event, book) =>{
-       //console.log(event.target.value)
-        console.log('hello')
-      //  alert(event.target.value + "book id = " + book.id + " and book list = " + book.shelf)
-        if(event.target.value === 'currentlyReading'){
-          this.moveToCurrentlyReading(book)
-        }
-        else if(event.target.value === 'read'){
-          this.moveToRead(book)
-        }
-        else if(event.target.value === 'wantToRead'){
-           this.moveToWantToRead(book)
-        }
-      }
+  handleChange = (event, book) =>{
+    //console.log(event.target.value)
+    console.log('hello')
+  //  alert(event.target.value + "book id = " + book.id + " and book list = " + book.shelf)
+    if(event.target.value === 'currentlyReading'){
+      this.moveToCurrentlyReading(book)
+    }
+    else if(event.target.value === 'read'){
+      this.moveToRead(book)
+    }
+    else if(event.target.value === 'wantToRead'){
+        this.moveToWantToRead(book)
+    }
+  }
 
-    moveToCurrentlyReading = (book) => {
-      this.setState((state) =>{
-        book.shelf = "currentlyReading"
+  moveToCurrentlyReading = (book) => {
+    this.setState((state) =>{
+      book.shelf = "currentlyReading"
+    })
+  }
+  moveToRead = (book) => {
+    this.setState((state) =>{
+        book.shelf = "read"
       
+    })
 
-      })
-    }
-    moveToRead = (book) => {
-      this.setState((state) =>{
-         book.shelf = "read"
-        
-      })
-
-    }
-    moveToWantToRead = (book) => {
-      this.setState((state) =>{
-         book.shelf = "wantToRead"
-        
-      })
-    }
-
-    render(){
-
+  }
+  moveToWantToRead = (book) => {
+    this.setState((state) =>{
+        book.shelf = "wantToRead"
       
-      //const books = this.props.books;
-       return(
-        <div className="list-books">
-            <div className="list-books-title">
-              <h1>My Reads</h1>
-            </div>        
-            <div className="list-books-content">
-                <div>
+    })
+  }
 
-                  
-                    <BookShelf handleChange={this.handleChange} moveToCurrentlyReading={this.moveToCurrentlyReading}   moveToRead={this.moveToRead} moveToWantToRead={this.moveToWantToRead}  books={this.state.books} listFormattedName='Currently Reading' listName='currentlyReading'/>
-                    <BookShelf handleChange={this.handleChange} moveToCurrentlyReading={this.moveToCurrentlyReading}   moveToRead={this.moveToRead} moveToWantToRead={this.moveToWantToRead}  books={this.state.books}  listFormattedName='Read' listName='read'/>
-                    <BookShelf  handleChange={this.handleChange} moveToCurrentlyReading={this.moveToCurrentlyReading}   moveToRead={this.moveToRead} moveToWantToRead={this.moveToWantToRead} books={this.state.books}   listFormattedName='Want to Read' listName='wantToRead'/>
-                  
-                </div>
-            </div>    
+  render(){
+
+    
+    //const books = this.props.books;
+    return(
+      <div className="list-books">
+        <div className="list-books-title">
+          <h1>My Reads</h1>
         </div>        
-        )
-    }
+        <div className="list-books-content">
+          <div>
+                       
+            <BookShelf handleChange={this.handleChange} moveToCurrentlyReading={this.moveToCurrentlyReading}   moveToRead={this.moveToRead} moveToWantToRead={this.moveToWantToRead}  books={this.state.books} listFormattedName='Currently Reading' listName='currentlyReading'/>
+            <BookShelf handleChange={this.handleChange} moveToCurrentlyReading={this.moveToCurrentlyReading}   moveToRead={this.moveToRead} moveToWantToRead={this.moveToWantToRead}  books={this.state.books}  listFormattedName='Read' listName='read'/>
+            <BookShelf  handleChange={this.handleChange} moveToCurrentlyReading={this.moveToCurrentlyReading}   moveToRead={this.moveToRead} moveToWantToRead={this.moveToWantToRead} books={this.state.books}   listFormattedName='Want to Read' listName='wantToRead'/>
+          
+          </div>
+        </div>              
+      </div>                
+    )
+  }
 }
  
     
